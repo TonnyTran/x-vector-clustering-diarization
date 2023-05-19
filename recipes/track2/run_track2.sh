a61733e4bb0beb7d4e8951bfc0e72dcc0117bcb8
TonnyTran <anhtt17895@gmail.com>
2023-05-19 18:43:45 +0800

    update

--- recipes/track2/run_track2.sh
+++ recipes/track2/run_track2.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-LD_LIBRARY_PATH="/home3/theanhtran/env/anaconda/envs/dihard_baseline/lib:$LD_LIBRARY_PATH"
-cmd="/home3/theanhtran/slurm.pl --quiet --nodelist=node06 --gpu 1 --nodelist=node06 --gpu 1" #  --nodelist=node06 --gpu 1
+cmd="/home3/theanhtran/slurm.pl --quiet --gpu 1" #  --nodelist=node06 --gpu 1
 
 . /home3/theanhtran/env/anaconda/etc/profile.d/conda.sh
-conda activate dihard_baseline
+conda activate xvector_diariazation
 
 $cmd log/track2.log \
 ./run.sh
